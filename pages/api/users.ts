e39158cdd42539b4c1aeb4016dbaed79dbd05001
douglas85rj <douglas85rj@gmail.com>
2022-12-16 19:41:17 -0300

    api/users

---
+++ pages/api/users.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient , User} from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+export default async function users(req: NextApiRequest, res:NextApiResponse){
+
+    const users = await prisma.user.findMany();
+
+res.status(200).json(users);
+}
