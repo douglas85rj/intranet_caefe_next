30d5a7ed3b4335de20dde4f3cddfdad9a46c581f
douglas85rj <douglas85rj@gmail.com>
2022-12-16 17:40:06 -0300

    Layout-grid_Blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,20 +1,53 @@
 
-import Link from 'next/link'
-import Layout from '../../src/components/layout'
-import BlogPost from './[slug]'
+import { GetStaticProps } from "next";
+import { apolloClient } from "src/apolloClient";
+import { decodePosts, PostsData } from "src/decoders/decodePosts";
+import { queryLatestPosts } from "src/queries/queryLatestPosts";
+import Layout from "../../src/components/layout";
 
+type PostsExamplePageProps = PostsData;
 
-export async function getStaticProps(){
-    const res = await fetch(
-        `https://webservices.jumpingcrab.com/posts`
-    )
-    const posts = await res.json()
-
-    return{
-        props: {
-            posts,
-        },
-    }
+export default function PostsExamplePage(props: PostsExamplePageProps) {
+  return (
+    <Layout pageTitle="Intranet">
+    <div className="feed">
+      {props.posts.map(({ title, image }) => (
+         <div className="feed-grid">
+          <img src={image} width={320} />
+          <h2>{title}</h2>
+        </div>
+        
+      ))}
+    </div>
+    </Layout>
+  );
 }
 
-export default BlogPost;
+export const getStaticProps: GetStaticProps<
+  PostsExamplePageProps
+> = async () => {
+  const { data } = await apolloClient.query({
+    query: queryLatestPosts,
+  });
+  const { posts } = decodePosts(data);
+  return {
+    props: {
+      posts,
+    },
+  };
+  <style jsx>{`
+        .feed {
+          padding: 8px 0;
+        }
+        .feed-grid {
+          display: grid;
+          gap: 16px;
+          grid-template-columns: repeat(3, 1fr);
+        }
+        @media (max-width: 600px) {
+          .feed-grid {
+            gap: 1px !important;
+          }
+        }
+      `}</style>
+};
