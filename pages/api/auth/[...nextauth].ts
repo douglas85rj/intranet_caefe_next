c17255fb067d22b538603f8a939311d82cbfe434
douglas85rj <douglas85rj@gmail.com>
2022-12-20 01:06:02 -0300

    Next_auth_provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,29 @@
 import NextAuth from "next-auth"
-
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   
   providers: [
+    CredentialsProvider({
    
+    name: "Credentials",
+    
+    credentials: {
+      username: { label: "Email", type: "text", placeholder: "" },
+      password: { label: "Senha", type: "password" }
+    },
+    async authorize(credentials, req) {
+        return null;
+     
+    },
+  })
   ],
-}
-
+};
+  
 export default NextAuth(authOptions)
+
+
+
+
+
+  
