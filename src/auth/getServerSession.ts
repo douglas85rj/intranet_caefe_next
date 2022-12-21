6dab9e7e8e8c6fd2976c4d7fe2c78444c270bf67
Eduardo Velho <eduardo.velho@icloud.com>
2022-12-21 10:24:55 -0300

    session fix

--- src/auth/getServerSession.ts
+++ src/auth/getServerSession.ts
@@ -1,15 +1,14 @@
 import type {
-    NextApiRequest,
-    NextApiResponse,
-    GetServerSidePropsContext,
-  } from "next";
-  import { authOptions } from "pages/api/auth/[...nextauth]";
-  import { getSession } from "next-auth/react";
-  
-  export function getServerSession(
-    req: NextApiRequest | GetServerSidePropsContext["req"],
-    res: NextApiResponse | GetServerSidePropsContext["res"]
-  ) {
-    return getSession();
+  NextApiRequest,
+  NextApiResponse,
+  GetServerSidePropsContext,
+} from "next";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth";
 
-  }
+export function getServerSession(
+  req: NextApiRequest | GetServerSidePropsContext["req"],
+  res: NextApiResponse | GetServerSidePropsContext["res"]
+) {
+  return unstable_getServerSession(req, res, authOptions);
+}
